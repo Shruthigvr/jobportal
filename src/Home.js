5f23efb95e5b127dc3ce60685e00b9510ea63011
shruthigvr <shruthigvr@gmail.com>
2019-03-26 16:11:05 +0530

    Second commit

--- src/Home.js
+++ src/Home.js
@@ -6,9 +6,12 @@ class Home extends Component {
 
     render() {
         return (
-            <div class="main">
-				<div id="listdiv">
-				</div>
+            <div>
+				<div className="topnav">				 
+  				<input type="text" placeholder="Search.."/>
+  				<button class="addbtn">New User</button>
+				
+			</div>
 			</div>
         );
     }

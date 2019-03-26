b9e7dd8f2e11a266a9f49fad7afc9b47d347c989
shruthigvr <shruthigvr@gmail.com>
2019-03-26 14:57:08 +0530

    first commit

---
+++ src/Home.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import './homestyle.css';
+
+class Home extends Component {
+
+    render() {
+        return (
+            <div class="main">
+				<div id="listdiv">
+				</div>
+			</div>
+        );
+    }
+}
+
+export default Home;
